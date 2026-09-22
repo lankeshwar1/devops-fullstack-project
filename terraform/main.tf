cbb726e22b510fe02d1ddb74c15d4c7c937a713e
lankeshwar1 <hippalitmus@gmail.com>
2026-09-22 23:27:40 +0530

    kubernetes & terraform setup in jenkins

---
+++ terraform/main.tf
@@ -0,0 +1,70 @@
+resource "kubernetes_namespace" "devops" {
+  metadata {
+    name = "devops"
+  }
+}
+
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name      = "devops-fullstack-app"
+    namespace = "devops"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "devops-fullstack-app"
+      }
+
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "devops-fullstack-app"
+        }        
+      }
+
+      spec {
+        container {
+          name              = "devops-fullstack-app"
+          image             = "devops-fullstack-app:3"
+          image_pull_policy = "IfNotPresent"
+
+          port {
+            container_port = 3000
+          }
+        }
+        
+      }
+
+
+    }
+    
+  }
+  
+}
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_service" "app" {
+  metadata {
+    name = "devops-fullstack-app"
+    namespace = "devops"
+  }
+
+  spec {
+    selector = {
+      app = "devops-fullstack-app"
+    }
+
+    port {
+      port = 3000
+      target_port = 3000
+    }
+    type = "NodePort"
+  } 
+}
